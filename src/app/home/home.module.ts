3c571b7dc5227c6216466671616548be48dac595
KarinBourass <bourasskarim9@gmail.com>
2022-05-25 22:45:30 +0100

    Refactor code

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,9 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeHeaderComponent } from './layout/home-header/home-header.component';
-import { HomeFooterComponent } from './layout/home-footer/home-footer.component';
 import { CarModelComponent } from './components/car-model/car-model.component';
-import { ModileSideMenuComponent } from './components/modile-side-menu/modile-side-menu.component';
 
 const routes: Routes = [
   {
@@ -18,14 +16,12 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     HomeComponent,
-    HomeHeaderComponent,
-    HomeFooterComponent,
     CarModelComponent,
-    ModileSideMenuComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    SharedModule
 
   ]
 })
