8a3cfcaeab36b3b6381f3f322c1cca06f1aa28b4
Karim Bourass <karim@Karims-MacBook-Pro.local>
2022-07-26 00:35:44 +0100

    Use tailwind animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  content: ["./components/**/*.{html,js}"],
+  content: ["./src/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        'raleway': ['Raleway', 'sans-serif']
+        raleway: ["Raleway", "sans-serif"],
+      },
+      keyframes: {
+        slidetoToTop: {
+          "0%": { opacity: "0", transform: "translateY(60px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        slidetoToTop: "slidetoToTop 1s",
       },
     },
   },
